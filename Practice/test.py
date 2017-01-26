8aa262915f94e55721a9191229a0dbaf49387865
heramareh <8926924@qq.com>
2017-01-26 21:51:47 +0800

    111

--- Practice/test.py
+++ Practice/test.py
@@ -1,6 +1,6 @@
 #encoding=utf-8
 
-import gloryroad
+# import gloryroad
 
 # if __name__ == '__main__':
 # #     print gloryroad.fun111()
@@ -22,37 +22,47 @@ import gloryroad
 '''写一个逐页显示文本文件的程序。
 每次显示文本文件的指定行数，
 暂停并向用户提示'按任意键继续'，按键后继续执行'''
-import os
-def get_content_by_lines(path,lines):
-    try:
-        if not os.path.exists(path):
-            raise IOError("path not exists!")
-        if not os.path.isfile(path):
-            raise IOError("path not a file!")
-        lineno = 0
-        with open(path) as fp:
-            for eachline in fp:
-                if lineno % lines == 0 and lineno != 0:
-                    os.system('pause')
-                print eachline,
-                lineno += 1
-    except Exception,e:
-        print str(e)
+# import os
+# def get_content_by_lines(path,lines):
+#     try:
+#         if not os.path.exists(path):
+#             raise IOError("path not exists!")
+#         if not os.path.isfile(path):
+#             raise IOError("path not a file!")
+#         lineno = 0
+#         with open(path) as fp:
+#             for eachline in fp:
+#                 if lineno % lines == 0 and lineno != 0:
+#                     os.system('pause')
+#                 print eachline,
+#                 lineno += 1
+#     except Exception,e:
+#         print str(e)
 
 # get_content_by_lines("d:\\test\\d.txt",5)
 
-import win32api,time
-import win32con
-# os.system('pause')
-while False:
-    print 1
-    # 回车
-    win32api.keybd_event(38,0,0,0)
-    win32api.keybd_event(38,0,win32con.KEYEVENTF_KEYUP,0)
-    time.sleep(0.1)
-    # 上方向键
-    win32api.keybd_event(13,0,0,0)
-    win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0)
-    time.sleep(0.1)
-    print 2
+# import win32api,time
+# import win32con
+# # os.system('pause')
+# while False:
+#     print 1
+#     # 回车
+#     win32api.keybd_event(38,0,0,0)
+#     win32api.keybd_event(38,0,win32con.KEYEVENTF_KEYUP,0)
+#     time.sleep(0.1)
+#     # 上方向键
+#     win32api.keybd_event(13,0,0,0)
+#     win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0)
+#     time.sleep(0.1)
+#     print 2
 
+def foo():
+    print "calling foo()..."
+    aString = 'bar'
+    anInt = 123
+    print "foo()'s globals:",globals().keys()
+    print "foo()'s locals:",locals().keys()
+a = 100
+print "__main__'s globals:",globals().keys()
+print "__main__'s locals:",locals().keys()
+foo()
