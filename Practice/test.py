9149c9d69ee97aa4e4046b0d1e78626ed2f1ae6a
heramareh <8926924@qq.com>
2017-03-03 13:19:36 +0800

    11

--- Practice/test.py
+++ Practice/test.py
@@ -1,4 +1,4 @@
-#encoding=utf-8
+# encoding=utf-8
 
 # import gloryroad
 
@@ -22,6 +22,8 @@
 '''写一个逐页显示文本文件的程序。
 每次显示文本文件的指定行数，
 暂停并向用户提示'按任意键继续'，按键后继续执行'''
+
+
 # import os
 # def get_content_by_lines(path,lines):
 #     try:
@@ -80,7 +82,8 @@
 
 class SortedKeyDict(dict):
     def keys(self):
-        return sorted(super(SortedKeyDict,self).keys())
+        return sorted(super(SortedKeyDict, self).keys())
+
 
 # d = SortedKeyDict((('a',1),('b',2),('c',3)))
 # print [key for key in d]
@@ -94,65 +97,69 @@ class SortedKeyDict(dict):
 
 # 修改文件夹下的所有文件及文件夹的修改时间
 import os
+
+
 def recname(path):
-    print "path:",path
+    print "path:", path
     os.system("pause")
     if os.path.exists(path):
-        for root,dirs,files in os.walk(path):
+        for root, dirs, files in os.walk(path):
             for dir in dirs:
-#                 print os.path.join(root,dir)
-                os.utime(os.path.join(root,dir),None)
+                #                 print os.path.join(root,dir)
+                os.utime(os.path.join(root, dir), None)
             for file in files:
-#                 print os.path.join(root,file)
-                os.utime(os.path.join(root,file),None)
+                #                 print os.path.join(root,file)
+                os.utime(os.path.join(root, file), None)
         print "done!"
     else:
         print "path not exists!"
     os.system("pause")
 
+
 # path = raw_input("please input path:")
 # recname(path)
 
-#修改文件夹下所有.java和.rftdef结尾的文件内容中指定字符串替换为新字符串
-def change(path,old_str,new_str):
-    print "path:",path
+# 修改文件夹下所有.java和.rftdef结尾的文件内容中指定字符串替换为新字符串
+def change(path, old_str, new_str):
+    print "path:", path
     os.system("pause")
     if os.path.exists(path):
-        for root,dirs,files in os.walk(path):
+        for root, dirs, files in os.walk(path):
             for file in files:
-                if os.path.splitext(file)[1] in ['.java','.rftdef']:
-                    file_path = os.path.join(root,file)
+                if os.path.splitext(file)[1] in ['.java', '.rftdef']:
+                    file_path = os.path.join(root, file)
                     content = ''
-                    with open(file_path,'rb') as fp:
+                    with open(file_path, 'rb') as fp:
                         content = fp.read()
                         content_list = content.split(old_str)
                         content = new_str.join(content_list)
-                    with open(file_path,'wb') as fp:
+                    with open(file_path, 'wb') as fp:
                         fp.write(content)
         print "done!"
     else:
         print "path not exists!"
     os.system("pause")
 
-#删除目录下所有指定的目录名及目录下的文件和文件夹
-def deletedir(path,dirname):
-    print "path:",path
+
+# 删除目录下所有指定的目录名及目录下的文件和文件夹
+def deletedir(path, dirname):
+    print "path:", path
     os.system("pause")
     if os.path.exists(path):
-        for root,dirs,files in os.walk(path):
+        for root, dirs, files in os.walk(path):
             for dir in dirs:
                 if dir == dirname:
                     dir_path = os.path.join(root, dir)
                     print dir_path
-                    for root1,dirs1,files1 in os.walk(dir_path):
+                    for root1, dirs1, files1 in os.walk(dir_path):
                         print files1
                         for file1 in files1:
-                            file_path = os.path.join(root1,file1)
+                            file_path = os.path.join(root1, file1)
                             os.remove(file_path)
-                    for root2,dirs2,files2 in os.walk(dir_path,topdown = False):
+                    for root2, dirs2, files2 in os.walk(dir_path, topdown=False):
                         for dir in dirs2:
                             try:
-                                os.removedirs(os.path.join(root2,dir))
+                                os.removedirs(os.path.join(root2, dir))
                             except:
                                 continue
                     try:
@@ -164,16 +171,18 @@ def deletedir(path,dirname):
         print "path not exists!"
     os.system("pause")
 
+
 # path = raw_input("please input path:")
 # deletedir(path,'jiuqi')
 
 def x(n):
     global count
     count += 1
-    if n<=3:
+    if n <= 3:
         return 1
     else:
-        return x(n-2)+x(n-4)+1
+        return x(n - 2) + x(n - 4) + 1
+
 
 # count = 0
 # print x(8)
@@ -189,8 +198,6 @@ def x(n):
 #     print matchResult.group()
 # else:
 #     print "no string found!"
-
-import re
 
 # s = 'aaa111aaa , bbb222 , 333ccc'
 # # 指定前后肯定断言
@@ -211,7 +218,7 @@ import re
 #   print e
 # else:
 #   print matchResult
-  
+
 # s = 'aaa111aaa , bbb222 , 333ccc'
 # # 指定前后否定断言，不满足前三个字母和后三个字母的条件
 # print re.findall( r'(?<![a-z]{3})\d+(?![a-z]+)', s) 
@@ -231,8 +238,7 @@ import re
 #   print e
 # else:
 #   print matchResult
-  
-import openpyxl
+
 # wb = openpyxl.Workbook()
 # ws = wb.active
 # ws['A3'] = "hahaha"
@@ -261,25 +267,27 @@ import openpyxl
 # wb.save("d:\\test\\excel\\e2.xlsx")
 import string
 from openpyxl import Workbook
-from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
-def create_excel(lista,content_list):
+from openpyxl.styles import Border, Side, PatternFill, Font, Alignment
+
+
+def create_excel(lista, content_list):
     wb = Workbook()
     ws = wb.active
-    #边框
-    #粗细
+    # 边框
+    # 粗细
     thin1 = Side(border_style="thin", color="000000")
-    #单双
+    # 单双
     double1 = Side(border_style="double", color="000000")
-    #上下左右边框
+    # 上下左右边框
     border1 = Border(top=thin1, left=thin1, right=thin1, bottom=thin1)
-    #字体、是否粗体、颜色、大小
-    font1 = Font(name=u"微软雅黑",b=True, color="000000",size=12)
-    #对齐方式
+    # 字体、是否粗体、颜色、大小
+    font1 = Font(name=u"微软雅黑", b=True, color="000000", size=12)
+    # 对齐方式
     al1 = Alignment(horizontal="center", vertical="center")
-    #填充背景色
+    # 填充背景色
     fill1 = PatternFill("solid", fgColor="3090C7")
     border2 = Border(top=thin1, left=thin1, right=thin1, bottom=thin1)
-    font2 = Font(name=u"微软雅黑",b=False, color="000000",size=12)
+    font2 = Font(name=u"微软雅黑", b=False, color="000000", size=12)
     al2 = Alignment(horizontal="center", vertical="center")
     fill2 = PatternFill("solid", fgColor="FFFF00")
     ws.append(lista)
@@ -287,25 +295,26 @@ def create_excel(lista,content_list):
         ws.append(i)
     cols_num = string.ascii_uppercase[:len(lista)]
     for i in cols_num:
-        cell = ws[i+"1"]
-        cell.fill=fill1
-        cell.border=border1
-        cell.font=font1
-        cell.alignment=al1
-    for i in range(2,len(content_list)+2):
+        cell = ws[i + "1"]
+        cell.fill = fill1
+        cell.border = border1
+        cell.font = font1
+        cell.alignment = al1
+    for i in range(2, len(content_list) + 2):
         for j in cols_num:
-            cell = ws[j+str(i)]
-            cell.fill=fill2
-            cell.border=border2
-            cell.font=font2
-            cell.alignment=al2
+            cell = ws[j + str(i)]
+            cell.fill = fill2
+            cell.border = border2
+            cell.font = font2
+            cell.alignment = al2
 
     wb.save(u"d:\\test\\excel\\test.xlsx")
 
-lista = ["序号","姓名","性别","学号","爱好","年龄"]
-content = ["=row()-1","张三","男",1,"篮球",20]
+
+lista = ["序号", "姓名", "性别", "学号", "爱好", "年龄"]
+content = ["=row()-1", "张三", "男", 1, "篮球", 20]
 content_list = []
 for i in range(8):
     content_list.append(content)
 print content_list
-create_excel(lista,content_list)
+create_excel(lista, content_list)
